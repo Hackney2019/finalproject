26fa6a8dc9c270bb970b87656e6c06e91f0ec5bb
Hackney2019 <104803576+Hackney2019@users.noreply.github.com>
2022-07-25 12:22:32 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function displayTemperature(response) {
   console.log(response.data.main.temp);
 }
 
-let apiKey = "7c30cb10a35fd5546334634c6a01897d";
+let apiKey = "32af4edcba23e12e10d2fa362b950362";
 
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}";
