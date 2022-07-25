defe02e08ce68a456037760e010e673ee587220f
Hackney2019 <104803576+Hackney2019@users.noreply.github.com>
2022-07-25 16:37:56 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 function displayTemperature(response) {
   console.log(response.data.main.temp);
+  let url = "https://jsonplaceholder.typicode.com/users/1";
+  axios.get(url).then(showUser);
 }
 
 let apiKey = "32af4edcba23e12e10d2fa362b950362";
 
 let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}";
+  "http://api.openweathermap.org/data/2.5/weather?q=Grove City&APPID=32af4edcba23e12e10d2fa362b950362";
 
-axios.get(apiUrl).then(displayTemperature);
+console.log(apiUrl);
